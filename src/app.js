aa0cab5a4874d42d6ea7c8f21fda54fcf580a57d
Leo Chaves <leingas13@gmail.com>
2024-10-15 01:22:32 +0000

    js updated, now it's random

--- src/app.js
+++ src/app.js
@@ -6,6 +6,45 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Array of card suits
+  let suits = ["♥", "♦", "♣", "♠"];
+
+  // Array of card values
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  // generate the random item
+  function getRandomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  let randomSuit = getRandomItem(suits);
+  let randomValue = getRandomItem(values);
+
+  document.querySelector(".card-title-heart").innerHTML = randomSuit;
+  document.querySelector(".card-text-value").innerHTML = randomValue;
+  document.querySelector(".card-title-heart2").innerHTML = randomSuit;
+
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    document.querySelector(".card-title-heart").style.color = "red";
+    document.querySelector(".card-title-heart2").style.color = "red";
+    document.querySelector(".card-text-value").style.color = "red";
+  } else {
+    document.querySelector(".card-title-heart").style.color = "black";
+    document.querySelector(".card-title-heart2").style.color = "black";
+    document.querySelector(".card-text-value").style.color = "black";
+  }
 };
