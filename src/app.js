872e1416543335e6e9b58487dc7856c4121c4b82
Leo Chaves <leingas13@gmail.com>
2024-10-15 01:33:27 +0000

    +1 s finished

--- src/app.js
+++ src/app.js
@@ -5,11 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  // Array of card suits
+function generateCard() {
+  // iconos
   let suits = ["♥", "♦", "♣", "♠"];
 
-  // Array of card values
   let values = [
     "A",
     "2",
@@ -26,20 +25,21 @@ window.onload = function() {
     "K"
   ];
 
-  // generate the random item
+  // random item
   function getRandomItem(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
   }
 
+  // random icon and alue
   let randomSuit = getRandomItem(suits);
   let randomValue = getRandomItem(values);
 
-  //update card values in htlm
+  // Uupdate html
   document.querySelector(".card-title-heart").innerHTML = randomSuit;
   document.querySelector(".card-text-value").innerHTML = randomValue;
   document.querySelector(".card-title-heart2").innerHTML = randomSuit;
 
-  //to change colors
+  // color change
   if (randomSuit === "♥" || randomSuit === "♦") {
     document.querySelector(".card-title-heart").style.color = "red";
     document.querySelector(".card-title-heart2").style.color = "red";
@@ -49,4 +49,24 @@ window.onload = function() {
     document.querySelector(".card-title-heart2").style.color = "black";
     document.querySelector(".card-text-value").style.color = "black";
   }
+
+  // input values manually
+  let cardWidth = document.getElementById("widthInput").value || 18;
+  let cardHeight = document.getElementById("heightInput").value || 400;
+
+  // update card with value size
+  document.querySelector(".card").style.width = cardWidth + "rem";
+  document.querySelector(".card").style.height = cardHeight + "px";
+}
+
+window.onload = function() {
+  generateCard();
+
+  // button add
+  document
+    .getElementById("generateCardButton")
+    .addEventListener("click", generateCard);
+
+  // timer add
+  setInterval(generateCard, 10000);
 };
